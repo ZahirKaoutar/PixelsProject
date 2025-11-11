5e4636e7a31b32f822d67ea4a3ea8cc0bd31dc65
youcode safi <kaoutar@gmail.com>
2025-11-11 10:41:43 +0100

    creation de page home

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     "./pixels/**/*.html",
+      "./pixels/**/*.js",
+     
+    "./src/**/*.js"  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
