27ab4dd12d62dc2787dc5fdae8e34019b064f565
youcode safi <kaoutar@gmail.com>
2025-11-11 14:40:55 +0100

    fix merge

---
+++ pixels/file.js
@@ -0,0 +1,59 @@
+ 
+const filterselect= document.querySelector('.filter') 
+ const Cards=document.querySelector('.cards')
+ async function  getSelect()
+ {
+    const datagame=await fetch("https://debuggers-games-api.duckdns.org/api/games?page=1&limit=20")
+    const reponse =await datagame.json()
+    const res1=reponse.results
+
+
+
+
+
+  //  const datagame=await fetch("https://gamees-api.netlify.app/public/filteredGames.json")
+  //   const reponse =await datagame.json()
+
+    const allgenres = res1.flatMap(elem => elem.genres.map(g => g.name));
+    const genresUniques = [...new Set(allgenres)];
+
+    const allplateformes = res1.flatMap(elem => elem.parent_platforms.map(platf => platf.platform.name));
+    const palteformeUniques = [...new Set(allplateformes)];
+    
+  filterselect.innerHTML+=`<select  class="text-white  bg-black   ml-4 text-center w-20 rounded-3xl border border-white-300 p-2">  <option disabled selected"> genre</option>${genresUniques.map(e=>`<option value=${e}>${e}</option>`)}</select>`
+  filterselect.innerHTML+=`<select  class="text-white bg-black  ml-4 text-center w-[90px] rounded-3xl border border-white-300 p-2">  <option disabled selected> note</option>${palteformeUniques.map(e=>`<option value=${e}>${e}</option>`)}</select>`
+  filterselect.innerHTML+=`<select  class="text-white    bg-black ml-4 mr-3  text-center w-[110px] rounded-3xl border border-white-300 p-2">  <option disabled selected > plateforme</option>${palteformeUniques.map(e=>`<option value=${e}>${e}</option>`)}</select>`
+
+
+
+
+ }
+ getSelect()
+
+
+async function getcard(){
+   
+    const datagame=await fetch("https://debuggers-games-api.duckdns.org/api/games?page=1&limit=20")
+    const reponse =await datagame.json()
+    const res1=reponse.results
+
+   Cards.innerHTML += res1.map(game => `
+      <div class='card  m-auto  mt-5 rounded-3xl w-[300px]'>
+        <div data-userid="${game.id}" class="card-img ">
+          <img  class="rounded-t-2xl"src="${game.background_image}">
+        </div>
+        <div class="card-text bg-black text-white rounded-b-lg h-12">
+
+
+            <div class="img-platform">
+                <img src=""><img src=""><img src="">
+            </div>
+            <div class="namecoeur  flex justify-between  gap-4">
+                 <h2 class="name text-white font-bold  ">${game.name}</h2>
+                <img  class="w-[20px]"   src="IMG/comme.png">
+            </div>
+        </div>
+      </div>
+   `).join(""); 
+}
+getcard();
